1af7cc3510beccefae8337f10ff67d23d10ff73b
RiyadSheikh27 <riyad.cse27@gmail.com>
2026-06-17 17:04:21 +0600

    media file uploaded

--- config/urls.py
+++ config/urls.py
@@ -14,14 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
 
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/', include('apps.apis.urls')),
+    path("admin/", admin.site.urls),
+    path("api/v1/", include("apps.apis.urls")),
     path("ckeditor5/", include("django_ckeditor_5.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.BASE_DIR / "static"
+    )
