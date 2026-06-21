bcf69c120825fc459ccb8a9e7aa6751e3e656874
RiyadSheikh27 <riyad.cse27@gmail.com>
2026-06-21 18:31:53 +0600

    eligibility check during place order

--- config/settings.py
+++ config/settings.py
@@ -117,24 +117,24 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
-# Production Database
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'betopia_daily',
-#         'USER': 'betopia_daily_user',
-#         'PASSWORD': 'betopia_daily@123',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
 #     }
 # }
+
+# Production Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'betopia_daily',
+        'USER': 'betopia_daily_user',
+        'PASSWORD': 'betopia_daily@123',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
